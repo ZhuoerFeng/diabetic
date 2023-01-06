17db63c70c79c7d9ec9f9c77890770d9adf4b2e6
范喆恒 <2018010135@admin.secoder.net>
2023-01-06 11:00:55 +0800

    dunn

--- src/metrics/internal_metrics.py
+++ src/metrics/internal_metrics.py
@@ -25,26 +25,6 @@ def separation(data, pred):
     return np.sum(centroid_distances) / (n_labels * (n_labels - 1))
 
 
-def dunn_validity_index_score(data, pred):
-    n_labels = np.max(pred) + 1
-    max_intra_dists = np.zeros(n_labels)
-    min_extra_dists = np.full((n_labels, n_labels), np.inf)
-
-    dists = pairwise_distances(data)
-    
-    for k in range(n_labels):
-        pos_k = pred == k
-        max_intra_dists[k] = np.max(dists[pos_k][:, pos_k])
-
-    for i in range(n_labels):
-        pos_i = pred == i
-        for j in range(i):
-            dists_ij = dists[pos_i][:, pred == j]
-            min_extra_dists[i, j] = np.min(dists_ij[np.nonzero(dists_ij)])
-    
-    return np.min(min_extra_dists) / np.max(max_intra_dists)
-
-
 def SSE(data, pred):
     n_labels = np.max(pred) + 1
     sse = 0
@@ -53,3 +33,22 @@ def SSE(data, pred):
         centroid = cluster_k.mean(axis=0)
         sse += np.sum((cluster_k - centroid) ** 2)
     return sse
+
+from tqdm import tqdm
+
+def dunn_validity_index_score(data, pred):
+    n_labels = np.max(pred) + 1
+    max_intra_dists = np.zeros(n_labels)
+    min_extra_dists = np.full((n_labels, n_labels), np.inf)
+    
+    for k in range(n_labels):
+        pos_k = pred == k
+        max_intra_dists[k] = np.max(pairwise_distances(data[pos_k]))
+
+    for i in tqdm(range(n_labels)):
+        pos_i = pred == i
+        for j in range(i):
+            dists_ij = pairwise_distances(data[pos_i], data[pred == j])
+            min_extra_dists[i, j] = np.min(dists_ij[np.nonzero(dists_ij)])
+    print(min_extra_dists, max_intra_dists)
+    return np.min(min_extra_dists) / np.max(max_intra_dists)
